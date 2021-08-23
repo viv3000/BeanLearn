cddbadc379e0fdd36a5a2ae516933ff5494fa95c
viv3000 <viktor05012005@yandex.ru>
2021-08-24 01:40:21 +0300

    first

---
+++ src/main/java/com/BeanLearn/BeanLearnApplication.java
@@ -0,0 +1,13 @@
+package com.BeanLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BeanLearn/BeanLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeanLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
